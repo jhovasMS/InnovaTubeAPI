af989cd65541e204db2844b9e95ad5ebfe24c21d
jhovasMS <jhovany2244@gmail.com>
2025-05-22 01:58:29 -0600

    SistemaDeUsuarios corrección en JWT

--- InnovaTubeAPI/Program.cs
+++ InnovaTubeAPI/Program.cs
@@ -38,6 +38,8 @@ builder.Services.AddAuthentication().AddJwtBearer(opciones =>
     };
 });
 
+builder.Services.AddScoped<UserManager<IdentityUser>>();
+builder.Services.AddScoped<SignInManager<IdentityUser>>();
 
 var origenesPermitidos = builder.Configuration.GetValue<string>("OrigenesPermitidos")!.Split(",");
 
